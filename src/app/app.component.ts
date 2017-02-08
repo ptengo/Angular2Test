f2e0fb441532f0131caee1c40b2bb2ff9248a12c
ptengo <caraboniato@gmail.com>
2017-02-08 17:10:54 +0100

    Dashboard + navBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+import {Http} from '@angular/http';
+import {SidenavComponent} from './components/sidenav/sidenav.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'app works!';
+  pics: Array<any>;
+
+
+  constructor(private http:Http) {
+    
+    this.http.get('./data.json')
+      .map(response => response.json().screenshots)
+      .subscribe(res => this.pics = res);
+
+  }
+  randomName(start: any) {
+        start.toggle();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
