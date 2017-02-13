893e4b1b13c21051fbf83d1623a0f6b584c01215
ptengo <caraboniato@gmail.com>
2017-02-13 22:25:46 +0100

    2ms animate

--- src/app/animations/transition.animation.ts
+++ src/app/animations/transition.animation.ts
@@ -9,13 +9,13 @@ function slideOutAndIn():AnimationEntryMetadata {
     transition(
     ':enter', [
         style({transform: 'translateX(100%)', opacity: 0}),
-        animate('500ms', style({transform: 'translateX(0)',opacity: 1}))
+        animate('200ms', style({transform: 'translateX(0)',opacity: 1}))
     ]
     ),
     transition(
     ':leave', [
         style({transform: 'translateX(0)', 'opacity': 1}),
-        animate('500ms', style({transform: 'translateX(100%)',opacity: 0}),
+        animate('200ms', style({transform: 'translateX(100%)',opacity: 0}),
         )
     ]
     )]);
